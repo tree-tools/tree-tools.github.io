4bb0ba9b72a1aaa01c513de66faf86bb62de1146
shotaro <2b.sytr202@gmail.com>
2025-06-28 06:53:48 +0900

    add 記事一覧追加

---
+++ articles/data/articles.js
@@ -0,0 +1,30 @@
+const articlesData = [
+    {
+      id: "001",
+      title: "【GASで簡単自動化】ECサイトのCSVをスプレッドシートに自動転記するGASツール",
+      date: "2025-06-28",
+      category: "技術",
+      tags: ["GAS", "業務効率化", "スプレッドシート"],
+      link: "../posts/tools/ec-csv-to-spreadsheet/index.html",
+      thumbnail: "../../img/create01.jpg"
+    },
+    {
+      id: "002",
+      title: "コードまとめ",
+      date: "2025-06-24",
+      category: "技術",
+      tags: ["JS", "備忘録"],
+      link: "",
+      thumbnail: "../images/sample2.jpg"
+    },
+    {
+      id: "003",
+      title: "新しい記事のタイトル",
+      date: "2025-07-01",
+      category: "技術",
+      tags: ["JS", "Tips"],
+      link: "",
+      thumbnail: "../images/sample3.jpg"
+    }
+  ];
+  

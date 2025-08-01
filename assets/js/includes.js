870e9f81f7ac2f3e4f536af41631466c31918848
shotaro <2b.sytr202@gmail.com>
2025-08-01 18:48:44 +0900

    fix 共通パーツ読み込みJSを修正

--- assets/js/includes.js
+++ assets/js/includes.js
@@ -1,23 +1,21 @@
 // 共通パーツを読み込む関数
 document.addEventListener("DOMContentLoaded", () => {
-    includeHtml().then(() => {
-      initHeader(); 
-    });
+  includeHtml();
+});
+
+function includeHtml() {
+  const includeTargets = document.querySelectorAll("[data-include]");
+  const promises = [];
+
+  includeTargets.forEach(el => {
+    const file = el.getAttribute("data-include");
+    const p = fetch(file)
+      .then(response => response.text())
+      .then(data => {
+        el.innerHTML = data;
+      });
+    promises.push(p);
   });
-  
-  function includeHtml() {
-    const includeTargets = document.querySelectorAll("[data-include]");
-    const promises = [];
-  
-    includeTargets.forEach(el => {
-      const file = el.getAttribute("data-include");
-      const p = fetch(file)
-        .then(response => response.text())
-        .then(data => {
-          el.innerHTML = data;
-        });
-      promises.push(p);
-    });
-  
-    return Promise.all(promises);
-  }
+
+  return Promise.all(promises);
+}
